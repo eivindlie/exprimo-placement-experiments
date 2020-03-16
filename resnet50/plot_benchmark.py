d272746e8ef641cee18cca8a420364059e8931b5
Eivind Lie Andreassen <ela4991@gmail.com>
2020-03-16 11:46:40 +0100

    Update file paths

--- resnet50/plot_benchmark.py
+++ resnet50/plot_benchmark.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-benchmark_results = pd.read_csv('../../exprimo/experiment_results/sim_real_comp/benchmark_results.csv', header=None)
-real_times = pd.read_csv('../../exprimo/experiment_results/sim_real_comp/nets/scores.csv',
-                         names=['generation', 'Simulated time'], index_col='generation')
+benchmark_results = pd.read_csv('../../exprimo/experiment_results/sim_real_comp/resnet/benchmark_results.csv',
+                                header=None)
+simulated_times = pd.read_csv('../../exprimo/experiment_results/sim_real_comp/resnet/scores-half-bandwidth.csv',
+                              names=['generation', 'Simulated time'], index_col='generation')
 
 benchmark_avg = pd.DataFrame()
 benchmark_avg['generation'] = benchmark_results.iloc[:, 0].map(lambda x:
@@ -12,7 +12,7 @@ benchmark_avg['generation'] = benchmark_results.iloc[:, 0].map(lambda x:
 benchmark_avg['Real time (mean)'] = benchmark_results.iloc[:, 1:].mean(axis=1)
 benchmark_avg = benchmark_avg.set_index('generation').sort_index()
 
-all_scores = real_times.join(benchmark_avg, how='inner').sort_index()
+all_scores = simulated_times.join(benchmark_avg, how='inner').sort_index()
 
 ax = all_scores.plot()
 ax.set_xlabel('Generation')
