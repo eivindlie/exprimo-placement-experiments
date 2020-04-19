ca6ea4a70c957283effe64fd0beb0e3705385458
Eivind Lie Andreassen <ela4991@gmail.com>
2020-04-19 13:40:13 +0200

    Add option to normalize and average batch time plots

--- utils/plot_batch_training_times.py
+++ utils/plot_batch_training_times.py
@@ -2,9 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set()
+sns.set(style='whitegrid')
 
 FILE_PATH = '../../exprimo/experiment_results/sim_real_comp/resnet_benchmark_with_all_batches.csv'
+
+# Set whether to subtract and divide by average, yielding residuals as fraction of average training time
+NORMALIZE = True
+
+# Set whether to create individual plots of each generation, or the average over all generations
+PLOT_INDIVIDUAL = False
 
 
 def plot_times(data, title, output_file=None):
@@ -14,18 +20,27 @@ def plot_times(data, title, output_file=None):
     plt.title(title)
 
     if output_file:
-        plt.savefig(output_file)
+        plt.savefig(output_file, transparent=False)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    results = pd.read_csv(FILE_PATH, header=None)
+    results = pd.read_csv(FILE_PATH, header=None, index_col=0)
 
-    for i in range(results.shape[0]):
-        generation = results.iloc[i, 0].replace('gen_', '').replace('.json', '')
-        times = results.iloc[i, 1:]
+    if NORMALIZE:
+        means = results.mean(axis=1)
+        results = results.sub(means, axis=0).divide(means, axis=0)
 
-        output_file = f'../../exprimo/experiment_results/plots/batch_training_times/gen_{generation}.png'
+    if PLOT_INDIVIDUAL:
+        for i in range(results.shape[0]):
+            generation = results.index[0].replace('gen_', '').replace('.json', '')
+            times = results.iloc[i, 1:]
 
-        plot_times(times, f'Generation {generation}', output_file)
+            output_file = f'../../exprimo/experiment_results/plots/batch_training_times/gen_{generation}.png'
+
+            plot_times(times, f'Generation {generation}', output_file)
+    else:
+        avg_results = results.mean(axis=0)
+        plot_times(avg_results, 'Average batch time residuals',
+                   output_file='../../exprimo/experiment_results/plots/batch_training_times/average.png')
